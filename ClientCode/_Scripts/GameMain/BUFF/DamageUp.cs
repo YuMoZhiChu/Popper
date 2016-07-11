35e332a6893b20b46d79a42fefc9aec9daaf15e4
zhiliang hu <zhiliang hu>
2016-07-11 19:42:17 +0800

    BUFF code

---
+++ ClientCode/_Scripts/GameMain/BUFF/DamageUp.cs
@@ -0,0 +1,44 @@
+﻿/**
+* ======== All the code changes must be recorded below. ========
+* 
+* ==============================================
+*/
+using UnityEngine;
+using System.Collections;
+
+public class DamageUp : PlayerBuff {
+
+	// Add public variables here:
+	public float DamageIncrease = 0.0f;
+	public float DamageIncreasePercentage = 1.0f;
+
+	// Add private members here:
+
+	// Add member functions here:
+	public override void StartBuff()
+	{
+		base.StartBuff();
+		if (TPC != null)
+		{
+			TPC.roleAttr.weaponAttr.normalFireDamage *= DamageIncreasePercentage;
+			TPC.roleAttr.weaponAttr.normalFireDamage += DamageIncrease;
+		}
+
+	}
+
+	public override void EndBuff()
+	{
+		base.EndBuff();
+		if (TPC != null)
+		{
+			TPC.roleAttr.weaponAttr.normalFireDamage -= DamageIncrease;
+			TPC.roleAttr.weaponAttr.normalFireDamage /= DamageIncreasePercentage;
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		base.Init();
+	}
+}
---
+++ ClientCode/_Scripts/GameMain/BUFF/HPRecover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPRecover : PlayerBuff {
+
+    public float recoverRate = 0.5f;
+
+    // 开始影响
+    override public void StartBuff()
+    {
+        float hp = TPC.hpControl.currentHP + TPC.hpControl.HP * recoverRate;
+        if (hp > TPC.hpControl.HP)
+            hp = TPC.hpControl.HP;
+        TPC.hpControl.currentHP = hp;
+        Destroy(this);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        base.Init();
+    }
+
+}
