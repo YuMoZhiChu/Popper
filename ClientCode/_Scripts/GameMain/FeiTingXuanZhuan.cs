62caf682022b60df5bf830ff04159eb98d2711aa
Wooze5401 <1158322330@qq.com>
2016-07-13 22:21:18 +0800

    reborn code

---
+++ ClientCode/_Scripts/GameMain/FeiTingXuanZhuan.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class FeiTingXuanZhuan : MonoBehaviour {
+
+	public float time = 300;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.DORotate (new Vector3 (0, -360, 0), time, RotateMode.FastBeyond360).SetEase (Ease.Linear).SetLoops (1000);
+	}
+
+}
