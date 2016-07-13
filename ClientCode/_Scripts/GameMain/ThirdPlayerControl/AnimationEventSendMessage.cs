1600cd512e924b5fdedc6454eea08c82d25767d7
coolile <coolile@outlook.com>
2016-07-13 21:45:10 +0800

    bob code5

---
+++ ClientCode/_Scripts/GameMain/ThirdPlayerControl/AnimationEventSendMessage.cs
@@ -0,0 +1,31 @@
+﻿/**
+* ======== All the code changes must be recorded below. ========
+* 
+* ==============================================
+*/
+using UnityEngine;
+using System.Collections;
+
+public class AnimationEventSendMessage : MonoBehaviour {
+
+	// Add public variables here:
+    public GameObject fatherNode;
+	
+	// Add private members here:
+	
+	// Add member functions here:
+    public void SendMessageToFatherNode(string function)
+    {
+        fatherNode.SendMessage(function);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
