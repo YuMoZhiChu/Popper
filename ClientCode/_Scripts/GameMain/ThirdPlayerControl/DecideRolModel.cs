3f10f1bbac6fb3b0fed39d15bae48ba6f3bd1b4d
coolile <coolile@outlook.com>
2016-07-14 19:32:53 +0800

    bob code7

---
+++ ClientCode/_Scripts/GameMain/ThirdPlayerControl/DecideRolModel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecideRolModel : MonoBehaviour {
+
+	public GameObject[] models;
+
+	// Use this for initialization
+	//void Awake() {
+	//	models[Datas.roleID].SetActive(true);
+	//	this.transform.GetComponent<ThirdPlayerControl> ().role = models[Datas.roleID];
+	//}
+	public void setRool(int roleId)
+	{
+		models[roleId].SetActive(true);
+		this.transform.GetComponent<ThirdPlayerControl>().role = models[roleId];
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
